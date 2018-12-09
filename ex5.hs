1ed172a7d0f5f6c5e0ad06b7fef08f19d7c17711
axel sarlin <ax.flumm@gmail.com>
2018-12-09 23:45:54 +0100

    modified:   ex5.hs

--- ex5.hs
+++ ex5.hs
@@ -1,19 +1,25 @@
 import Data.Char
 
 main = do
-    print $ length $ react "dabAcCaCBAcCcaDA"
-    print $ (react "dabAcCaCBAcCcaDA" == "dabCBAcaDA")
-    s <- readFile "input5.txt"
+    test
+    s <- readFile "input5.txt"          
     let t = filter (/= '\n') s
     print $ length $ react $  t
     print $ minimum $ map (length . react) $ map (removeletter t) alphabet
 
+test = do
+    let teststring = "dabAcCaCBAcCcaDA" 
+    let testanswer = react teststring
+    print $ length $ testanswer   
+    print $ (testanswer == "dabCBAcaDA") 
 
+
+-- iterate reaction until nothing happens
 react :: String -> String         
 react xs = if reaction xs == xs then xs
            else react $ reaction xs
 
-
+-- go through string and make all reactions happen
 reaction :: String -> String
 reaction [] = []
 reaction [x] = [x]
@@ -21,6 +27,6 @@ reaction (x:y:xs) = if (x /= y) && (toUpper x == toUpper y)
                     then reaction xs 
                     else x:(reaction (y:xs))
 
-
+-- other stuff needed
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 removeletter st c = filter (/= toLower c) $ filter (/= toUpper c) st
